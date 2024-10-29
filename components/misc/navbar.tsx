7b879d1905e0bb801d73ee59660bc88953c87537
Nikki <bravonikki13@gmail.com>
2024-10-29 10:44:15 +0100

    Refactor Navbar component to use semantic HTML elements

--- components/misc/navbar.tsx
+++ components/misc/navbar.tsx
@@ -7,8 +7,8 @@ import { Button } from '@/components/ui/button';
 
 export function Navbar() {
   return (
-    <div className='absolute z-50 flex w-full items-center justify-center p-4'>
-      <div className='h-10 w-full max-w-screen-lg rounded-xl bg-background/20 outline outline-1 outline-foreground/5 drop-shadow-xl backdrop-blur-xl'>
+    <header className='absolute z-50 flex w-full items-center justify-center p-4'>
+      <nav className='h-10 w-full max-w-screen-lg rounded-xl bg-background/20 outline outline-1 outline-foreground/5 drop-shadow-xl backdrop-blur-xl'>
         <div className='flex h-10 flex-row items-center justify-between sm:px-4'>
           <Button variant='link' asChild>
             <Link href='/'>Nikki</Link>
@@ -22,7 +22,7 @@ export function Navbar() {
             <ThemeSwitch />
           </div>
         </div>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 }
