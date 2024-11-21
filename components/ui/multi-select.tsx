bf18746ded71c6fdf4c43cfe33b8012a8de44388
Nikki <bravonikki13@gmail.com>
2024-11-21 16:11:34 +0100

    Added input and multi select component; Modified dropdown styles

---
+++ components/ui/multi-select.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+interface ISelectProps {
+  values: {
+    key: string;
+    value: string;
+  }[];
+  children?: React.ReactNode;
+  onSelectionChange?: (selectedItems: string[]) => void;
+}
+const MultiSelect = ({ values, children, onSelectionChange }: ISelectProps) => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const handleSelectChange = (value: string) => {
+    let updatedSelectedItems;
+    if (!selectedItems.includes(value)) {
+      updatedSelectedItems = [...selectedItems, value];
+    } else {
+      updatedSelectedItems = selectedItems.filter((item) => item !== value);
+    }
+    setSelectedItems(updatedSelectedItems);
+    if (onSelectionChange) {
+      onSelectionChange(updatedSelectedItems);
+    }
+  };
+
+  const isOptionSelected = (value: string): boolean => {
+    return selectedItems.includes(value) ? true : false;
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex gap-2 font-bold'>
+          {children}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-56'
+        onCloseAutoFocus={(e) => e.preventDefault()}
+      >
+        {values.map((value: ISelectProps['values'][0], index: number) => {
+          return (
+            <DropdownMenuCheckboxItem
+              onSelect={(e) => e.preventDefault()}
+              key={index}
+              checked={isOptionSelected(value.key)}
+              onCheckedChange={() => handleSelectChange(value.key)}
+            >
+              {value.value}
+            </DropdownMenuCheckboxItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default MultiSelect;
