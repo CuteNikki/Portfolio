aba1d1c4de7ec6a449e31fd3c5970b2076207386
Nikki <bravonikki13@gmail.com>
2024-11-26 09:24:30 +0100

    Add animated underline effect to navbar links

--- components/misc/navbar.tsx
+++ components/misc/navbar.tsx
@@ -3,21 +3,24 @@ import Link from 'next/link';
 import { routes } from '@/assets/routes';
 
 import { ThemeSwitch } from '@/components/theme/theme-switch';
-import { Button } from '@/components/ui/button';
 
 export function Navbar() {
   return (
     <header className='absolute z-50 flex w-full items-center justify-center p-4'>
       <nav className='h-10 w-full max-w-screen-lg rounded-md border border-foreground/10 bg-background/30 backdrop-blur-xl'>
         <div className='flex h-10 flex-row items-center justify-between'>
-          <Button variant='link' asChild>
-            <Link href='/'>Nikki</Link>
-          </Button>
+          <Link href='/' className='group rounded-md px-4 py-2'>
+            <span className='anim-underline'>Nikki</span>
+          </Link>
           <div className='flex flex-row'>
-            {routes.map((route) => (
-              <Button key={route.href} variant='link' asChild>
-                <Link href={route.href}>{route.name}</Link>
-              </Button>
+            {routes.map((route, index) => (
+              <Link
+                href={route.href}
+                key={`navbar-link-${index}-${route.name}-${route.href}`}
+                className='group rounded-md px-4 py-2'
+              >
+                <span className='anim-underline'>{route.name}</span>
+              </Link>
             ))}
             <ThemeSwitch />
           </div>
