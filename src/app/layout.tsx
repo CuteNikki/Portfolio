b290b19fc0489462a6bf8eaac087250e9e5e9265
Nikki <bravonikki13@gmail.com>
2025-03-31 15:20:24 +0200

    Recreated project with new versions of nextjs, shadcn and tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import { ThemeProvider } from '@/providers/theme';
-
-import { Footer } from '@/components/navigation/footer';
-import { Navbar } from '@/components/navigation/navbar';
-
 import './globals.css';
+
+import { ThemeProvider } from '@/components/theme/provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,13 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)] antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-          <main className='grid min-h-screen grid-rows-[auto,1fr,auto]'>
-            <Navbar />
-            {children}
-            <Footer />
-          </main>
+          <nav className='p-4'>Navbar</nav>
+          <main className='flex flex-1 flex-col'>{children}</main>
+          <footer className='p-4'>Footer</footer>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { ThemeButton } from '@/components/ui/theme-button';
 import Image from 'next/image';
+
+import { ThemeButton } from '@/components/theme/button';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20'>
-      <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
+    <div className='grid flex-1 grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20'>
+      <div className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
         <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
         <ol className='list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left'>
           <li className='mb-2'>
@@ -33,8 +34,8 @@ export default function Home() {
           </a>
           <ThemeButton />
         </div>
-      </main>
-      <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
+      </div>
+      <div className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
         <a
           className='flex items-center gap-2 hover:underline hover:underline-offset-4'
           href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
@@ -62,7 +63,7 @@ export default function Home() {
           <Image aria-hidden src='/globe.svg' alt='Globe icon' width={16} height={16} />
           Go to nextjs.org →
         </a>
-      </footer>
+      </div>
     </div>
   );
 }
