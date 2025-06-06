ebe1e7d43e22db262158cd173607073a4734b7d7
Nikki <bravonikki13@gmail.com>
2025-06-06 11:03:14 +0200

    Added Discord icon to the button

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,6 +14,7 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { DiscordIcon } from '@/components/ui/icons';
 
 export default function AboutPage() {
   const copyToClipboard = (text: string, label: string) => {
@@ -95,8 +96,9 @@ export default function AboutPage() {
 
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                      <Button variant='outline' size='sm' className='gap-1.5'>
-                        <span>Discord</span>
+                      <Button variant='outline' size='sm'>
+                        <DiscordIcon />
+                        Discord
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
