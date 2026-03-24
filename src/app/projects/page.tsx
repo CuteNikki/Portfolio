92f3c9d183e654978bb38570d590bb09335d6969
Nikki <bravonikki13@gmail.com>
2026-03-24 09:38:56 +0100

    feat: update Projects page layout with header and styling enhancements

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,21 @@
 import { SITE_METADATA } from '@/constants/metadata';
+import { PresentationIcon } from 'lucide-react';
 
 export const metadata = SITE_METADATA.projects;
 
 export default function Projects() {
-  return <div>Projects Page</div>;
+  return (
+    <div>
+      <div className='flex w-full max-w-5xl flex-col gap-4 border p-4 sm:p-8'>
+        <h1 className='text-primary-text flex items-center gap-2 text-xl font-semibold'>
+          <PresentationIcon className='shrink-0' /> Projects
+        </h1>
+      </div>
+      <div>
+        {/* {projects.map((project) => (
+          <div key={project.id} className='rounded-md border p-4'></div>
+        ))} */}
+      </div>
+    </div>
+  );
 }
