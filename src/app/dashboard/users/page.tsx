2a20fcb137c1231a378e607b4a7440651793c9c1
Nikki <bravonikki13@gmail.com>
2026-03-27 10:00:46 +0100

    fix: remove max-width restriction from UsersPage container

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -4,7 +4,7 @@ import { UsersTable } from '@/components/dashboard/user-table';
 
 export default async function UsersPage() {
   return (
-    <div className='flex w-full max-w-5xl flex-col gap-4 border p-4 sm:p-8'>
+    <div className='flex w-full flex-col gap-4 border p-4 sm:p-8'>
       <h1 className='text-primary-text flex items-center gap-2 text-xl font-semibold'>
         <Users2Icon className='shrink-0' /> Users
       </h1>
