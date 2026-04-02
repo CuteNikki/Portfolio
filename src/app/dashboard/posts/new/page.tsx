86837bed69232161ca0fff0e29e237a46fc9b90f
Nikki <bravonikki13@gmail.com>
2026-04-02 12:40:59 +0200

    feat: refactor NewPostPage layout to utilize Card components for improved structure

--- src/app/dashboard/posts/new/page.tsx
+++ src/app/dashboard/posts/new/page.tsx
@@ -4,18 +4,23 @@ import { SITE_METADATA } from '@/constants/metadata';
 
 import { NewPostForm } from '@/components/dashboard/posts/new';
 import { DashboardHeader } from '@/components/dashboard/shared/header';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 export const { dashboardNewPost: metadata } = SITE_METADATA;
 
 export default function NewPostPage() {
   return (
-    <div className='flex w-full max-w-5xl flex-col gap-4 justify-self-center border p-4 sm:p-8'>
-      <DashboardHeader
-        icon={NewspaperIcon}
-        title={'Create new Post'}
-        description={'Draft and publish a new post.'}
-      />
-      <NewPostForm />
-    </div>
+    <Card className='w-full max-w-5xl'>
+      <CardHeader>
+        <DashboardHeader
+          icon={NewspaperIcon}
+          title={'Create new Post'}
+          description={'Draft and publish a new post.'}
+        />
+      </CardHeader>
+      <CardContent>
+        <NewPostForm />
+      </CardContent>
+    </Card>
   );
 }
