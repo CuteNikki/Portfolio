0349da0d7492f53134561795d3f79602fabb68ea
Nikki <bravonikki13@gmail.com>
2026-04-02 12:40:49 +0200

    feat: refactor NewProjectPage layout to utilize Card components for improved structure

--- src/app/dashboard/projects/new/page.tsx
+++ src/app/dashboard/projects/new/page.tsx
@@ -4,18 +4,23 @@ import { SITE_METADATA } from '@/constants/metadata';
 
 import { NewProjectForm } from '@/components/dashboard/projects/new';
 import { DashboardHeader } from '@/components/dashboard/shared/header';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 export const { dashboardNewProject: metadata } = SITE_METADATA;
 
 export default function NewProjectPage() {
   return (
-    <div className='flex w-full max-w-5xl flex-col gap-4 justify-self-center border p-4 sm:p-8'>
-      <DashboardHeader
-        icon={PresentationIcon}
-        title={'Create new Project'}
-        description={'Draft and publish a new project.'}
-      />
-      <NewProjectForm />
-    </div>
+    <Card className='w-full max-w-5xl'>
+      <CardHeader>
+        <DashboardHeader
+          icon={PresentationIcon}
+          title={'Create new Project'}
+          description={'Draft and publish a new project.'}
+        />
+      </CardHeader>
+      <CardContent>
+        <NewProjectForm />
+      </CardContent>
+    </Card>
   );
 }
