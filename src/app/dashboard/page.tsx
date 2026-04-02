efa1f68b871b5b85a7b6912e0233039d9a962e7a
Nikki <bravonikki13@gmail.com>
2026-04-02 11:09:17 +0200

    feat: enhance dashboard with post views pie chart and error handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,69 @@
 import { SITE_METADATA } from '@/constants/metadata';
 
+import { ErrorToast } from '@/components/common/error-toast';
+import { PostChartPie } from '@/components/dashboard/posts/chart';
 import { SidebarInset } from '@/components/ui/sidebar';
+import prisma from '@/lib/prisma';
 
 export const { dashboard: metadata } = SITE_METADATA;
 
-export default function Page() {
+export default async function DashboardPage() {
+  const { posts, hasPostsError } = await prisma.post
+    .findMany({ orderBy: { views: 'desc' }, take: 5 })
+    .then((posts) => ({ posts, hasPostsError: false }))
+    .catch((error) => {
+      console.error('Error fetching posts:', error);
+      return { posts: [], hasPostsError: true };
+    });
+  // const { projects, hasProjectsError } = await prisma.project
+  //   .findMany()
+  //   .then((projects) => ({ projects, hasProjectsError: false }))
+  //   .catch((error) => {
+  //     console.error('Error fetching projects:', error);
+  //     return { projects: [], hasProjectsError: true };
+  //   });
+
   return (
     <SidebarInset>
-      <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-        <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-          <div className='bg-muted/50 aspect-video rounded-xl' />
-          <div className='bg-muted/50 aspect-video rounded-xl' />
-          <div className='bg-muted/50 aspect-video rounded-xl' />
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Welcome to the Dashboard</h1>
+        {/* {hasProjectsError && (
+        <ErrorToast
+          title='Error fetching projects'
+          description='There was an error while fetching the projects. Please try again later.'
+        />
+      )} */}
+        {hasPostsError && (
+          <ErrorToast
+            title='Error fetching posts'
+            description='There was an error while fetching the posts. Please try again later.'
+          />
+        )}
+        <div className='flex flex-wrap gap-4'>
+          {posts.length > 0 && (
+            <PostChartPie
+              chartData={posts.map((post, index) => ({
+                title: post.title,
+                views: post.views,
+                fill: `var(--color-post${index})`,
+              }))}
+              chartConfig={{
+                views: {
+                  label: 'Views',
+                },
+                ...Object.fromEntries(
+                  posts.map((post, index) => [
+                    `post${index}`,
+                    {
+                      label: post.title,
+                      color: `var(--chart-${(index % 5) + 1})`,
+                    },
+                  ]),
+                ),
+              }}
+            />
+          )}
         </div>
-        <div className='bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min' />
       </div>
     </SidebarInset>
   );
